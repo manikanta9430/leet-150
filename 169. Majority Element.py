5f90a934075fb9909113eb1cbc8dbf0cd248ad8c
manikanta9430 <manikanta.rayala9@gmail.com>
2025-04-16 13:24:19 -0400

    169

---
+++ 169. Majority Element.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        ans = nums[0]
+        count = 1
+
+        for i in range(1,len(nums)):
+            if nums[i] == ans:
+                count += 1
+            else:
+                count -= 1
+            if count == 0:
+                ans = nums[i]
+                count = 1
+        
+        return ans
+        
